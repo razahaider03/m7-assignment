e45d78fe1c33f19f52dde659335ba2a08effdb95
Raza Haider <razahaider03@gmail.com>
2020-09-07 14:55:46 +0530

    build and house

--- Building.java
+++ Building.java
@@ -1,8 +1,51 @@
-/**
- * This is the Building class file.
- */
+/** This is the Building class file. */
 public class Building {
+
+  // TODO - Put your code here.
+  private int mLength;
+  private int mWidth;
+  private int mLotLength;
+  private int mLotWidth;
+
+  private building(int length, int width, int lotlength, int lotwidth) {
+    this.length = mLength;
+    this.width = mWidth;
+    this.lotlength = mLotLength;
+    this.lotwidth = mLotWidth;
+  }
+
+  public int getLength() {
+    return this.mLength;
+  }
+
+  public int getWidth() {
+    return this.mWidth;
+  }
+  public int getLotLength(){
+    return this.mLotLength;
+  }
+  public int getLotWidth(){
+    return this.mLotWidth;
+  }
+  public void setLength(int){
     
-    // TODO - Put your code here.
+  }
+  public void setWidth(int){
     
+  }
+  public void setLotLength(int){
+    
+  }
+  public void setLotWidth(int){
+    
+  }
+  public int calcBuildingArea(){
+    
+  }
+  public int calcLotArea(){
+    
+  }
+  public String toString(){
+    
+  }
 }
--- House.java
+++ House.java
@@ -1,9 +1,25 @@
-/**
- * This is the House class file that extends Building.
- */
-public class House 
-       extends Building {
-       
-    // TODO - Put your code here.
-    
+/** This is the House class file that extends Building. */
+public class House extends Building {
+
+  // TODO - Put your code here.
+  private String mOwner;
+  private boolean mPool;
+
+  public House(int length, int width, int lotLength, int lotWidth) {}
+
+  public House(int length, int width, int lotLength, int lotWidth, String owner) {}
+
+  public House(int length, int width, int lotLength, int lotWidth, String owner, boolean pool) {}
+
+  public String getOwner() {}
+
+  public boolean hasPool() {}
+
+  public void setOwner(String name) {}
+
+  public void setPool(boolean bool) {}
+
+  public String toString() {}
+
+  public boolean equals(Object obj) {}
 }
